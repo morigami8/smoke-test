f3423a4eed90c9fffd14a7bb2070fd054ba79aab
Morgan Atwood <atwood.morganj@gmail.com>
2025-01-04 11:47:12 -0800

    handle client data

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -32,4 +33,33 @@ func main() {
 	}
 
 	wg.Wait()
+}
+
+func handleConnection(conn net.Conn, wg *sync.WaitGroup) {
+	defer conn.Close()
+	defer wg.Done()
+
+	buff := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buff)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Printf("error reading from client: %v", err)
+			return
+		}
+
+		_, err = conn.Write(buff[:n])
+
+		if err != nil {
+			fmt.Printf("error writing to client: %v", err)
+			return
+		}
+
+	}
+
 }
